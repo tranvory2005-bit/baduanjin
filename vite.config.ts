637c89550638c3936236744bcfad6c1ae7611b73
tranvory2005-bit <tranvory2005@gmail.com>
2026-07-12 14:25:09 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/taiji-app/', // <-- Change this to match your EXACT GitHub repository name
+  base: '/baduanjin-app/', // <-- Change this to match your EXACT GitHub repository name
 });
